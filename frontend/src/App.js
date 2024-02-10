75acaad6de4e9cd037b235b28a8aed237699ee35
Sho Sugihara <shosmail31@gmail.com>
2024-02-09 16:48:33 -0800

    added frontend input form and display with tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,47 @@
 import "./App.css";
+import React from "react";
 
 function App() {
+  const [longUrl, setLongUrl] = React.useState("");
+
+  const submitLongUrl = (event) => {
+    event.preventDefault();
+    const givenUrl = longUrl;
+    setLongUrl("");
+  };
+
   return (
-    <div className="App">
-      <h1>URL Shortener</h1>
-    </div>
+    <>
+      <header>
+        <h1>URL Shortener</h1>
+      </header>
+      <section className="App">
+        <h2>Input URL to be shortened</h2>
+        <form onSubmit={submitLongUrl}>
+          <label htmlFor="urlInput">
+            <input
+              name="urlInput"
+              type="text"
+              data-testid="long-url-input"
+              placeholder="input long URL here..."
+              value={longUrl}
+              onChange={(e) => {
+                setLongUrl(e.value);
+              }}
+            />
+          </label>
+          <button data-testid="shorten-url-button" type="submit">
+            Shorten URL
+          </button>
+        </form>
+      </section>
+      <section>
+        <h3>Shortened URL</h3>
+        <p data-testid="display-shortend-url"></p>
+        <h3>Original Long URL</h3>
+        <p data-testid="display-long-url"></p>
+      </section>
+    </>
   );
 }
 
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,58 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("renders", () => {
+  test("url shortener header", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/url shortener/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("form input for long url", () => {
+    render(<App />);
+    const urlInput = screen.getByPlaceholderText("input long URL here...");
+    expect(urlInput).toBeInTheDocument();
+  });
+
+  test("button to shorten long url", () => {
+    render(<App />);
+    const shortenButton = screen.getByTestId("shorten-url-button");
+    expect(shortenButton).toBeInTheDocument();
+  });
+
+  test("shortened url display at first render to be empty", () => {
+    render(<App />);
+    const displayShortURL = screen.getByTestId("display-shortend-url");
+    expect(displayShortURL).toBeEmpty();
+  });
+
+  test("long url display to be empty on first render", () => {
+    render(<App />);
+    const displayLongURL = screen.getByTestId("display-long-url");
+    expect(displayLongURL).toBeEmpty();
+  });
+});
+
+describe("given url shortner form", () => {
+  test("input value displays correctly on change", () => {
+    render(<App />);
+    const input = screen.getByTestId("long-url-input");
+    expect(input).toBeEmpty();
+    fireEvent.change(input, { target: { value: "exampleURL.com" } });
+    expect(input.value).toBe("exampleURL.com");
+    fireEvent.change(input, { target: { value: "exampleURL" } });
+    expect(input.value).toBe("exampleURL");
+  });
+
+  test("input value clears on shorten button click", () => {
+    render(<App />);
+
+    const input = screen.getByTestId("long-url-input");
+    const submitBtn = screen.getByTestId("shorten-url-button");
+
+    expect(input).toBeEmpty();
+    fireEvent.change(input, { target: { value: "exampleURL.com" } });
+    fireEvent.click(submitBtn);
+    expect(input.value).toBe("");
+  });
 });
